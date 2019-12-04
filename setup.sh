1ad62a1ce5332797269f6146e4fd6cf4dac24fec
opuyoshi <tokemeigaii@gmail.com>
2019-12-04 23:33:48 +0900

    add font setting

--- setup.sh
+++ setup.sh
@@ -12,3 +12,11 @@ for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+curl -# -O -L https://github.com/miiton/Cica/releases/download/v5.0.1/Cica_v5.0.1_with_emoji.zip
+unzip Cica_v5.0.1_with_emoji.zip
+mkdir -p /usr/local/share/fonts
+sudo cp Cica-{Bold, BoldItalic, Regular, RegularItalic}.ttf /usr/local/share/fonts/
+rm Cica-{Bold, BoldItalic, Regular, RegularItalic}.ttf 
+sudo fc-cache -vf
+fc-list | grep Cica
