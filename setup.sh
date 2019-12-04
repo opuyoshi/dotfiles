e0e9d7b6898f2f728b797645de0f2d58f53d11fb
opuyoshi <tokemeigaii@gmail.com>
2019-12-05 00:37:04 +0900

    add removing LICENSE.txt and COPYRIGHT.txt

--- setup.sh
+++ setup.sh
@@ -19,6 +19,8 @@ sudo mkdir -p /usr/local/share/fonts
 sudo cp *.ttf /usr/local/share/fonts/
 cd ~/dotfiles
 rm *.ttf 
+rm LICENSE.txt
+rm COPYRIGHT.txt
 rm Cica_v5.0.1_with_emoji.zip
 sudo fc-cache -vf
 fc-list | grep Cica
