930db3e71d5817a555b6ffed644b2ba718073a65
opuyoshi <tokemeigaii@gmail.com>
2019-12-05 00:32:56 +0900

    remove error about copying tff files

--- setup.sh
+++ setup.sh
@@ -16,9 +16,9 @@ done
 curl -# -O -L https://github.com/miiton/Cica/releases/download/v5.0.1/Cica_v5.0.1_with_emoji.zip
 unzip Cica_v5.0.1_with_emoji.zip
 sudo mkdir -p /usr/local/share/fonts
-sudo cp Cica-{Bold, BoldItalic, Regular, RegularItalic}.ttf /usr/local/share/fonts/
+sudo cp *.ttf /usr/local/share/fonts/
 cd ~/dotfiles
-rm Cica-{Bold, BoldItalic, Regular, RegularItalic}.ttf 
+rm *.ttf 
 rm Cica_v5.0.1_with_emoji.zip
 sudo fc-cache -vf
 fc-list | grep Cica
