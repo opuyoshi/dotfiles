017c975860fc30ad464146eec813ea0ff92a7381
opuyoshi <tokemeigaii@gmail.com>
2020-03-04 17:34:25 +0900

    modify for Ubuntu

--- setup.sh
+++ setup.sh
@@ -6,21 +6,9 @@ curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >
 sh ./installer.sh ~/dotfiles/.vim/dein
 cd ~/dotfiles
 
-DOT_FILES=(.vimrc .vim .latexmkrc .config)
+DOT_FILES=(.vimrc .vim .config)
 
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
-
-curl -# -O -L https://github.com/miiton/Cica/releases/download/v5.0.1/Cica_v5.0.1_with_emoji.zip
-unzip Cica_v5.0.1_with_emoji.zip
-sudo mkdir -p /usr/local/share/fonts
-sudo cp *.ttf /usr/local/share/fonts/
-cd ~/dotfiles
-rm *.ttf 
-rm LICENSE.txt
-rm COPYRIGHT.txt
-rm Cica_v5.0.1_with_emoji.zip
-sudo fc-cache -vf
-fc-list | grep Cica
