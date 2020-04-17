853e9f4fbd6852d81610ae3af44e8d807167fed7
opuyoshi <tokemeigaii@gmail.com>
2020-04-17 15:25:57 +0900

    .latexmkrcのシンボリックリンクを追加

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >
 sh ./installer.sh ~/dotfiles/.vim/dein
 cd ~/dotfiles
 
-DOT_FILES=(.vimrc .vim)
+DOT_FILES=(.vimrc .vim .latexmkrc)
 
 for file in ${DOT_FILES[@]}
 do
