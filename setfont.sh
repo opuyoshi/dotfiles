187a112eb3927e005dd611f2d7c64a01c0151d16
opuyoshi <33128761+opuyoshi@users.noreply.github.com>
2020-06-10 15:50:10 +0900

    fix typo

--- setfont.sh
+++ setfont.sh
@@ -1,8 +1,8 @@
-curl -# -O -L https://github.com/miiton/Cica/releases/download/Cica_v5.0.1_with_emoji.zip
+curl -# -O -L https://github.com/miiton/Cica/releases/download/v5.0.1/Cica_v5.0.1_with_emoji.zip
 unzip Cica_v5.0.1_with_emoji.zip
 sudo mkdir -p /usr/local/share/fonts
 sudo cp *.ttf /usr/local/share/fonts/
-cp ~/dotfiles
+cd ~/dotfiles
 rm *.ttf
 rm LICENSE.txt
 rm COPYRIGHT.txt
